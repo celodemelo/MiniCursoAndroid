4f732fb2fd946fd07840c21108d6ad23655c4ad6
Marcelo Madeira <celodemelo@gmail.com>
2013-05-19 18:31:06 -0300

    Initial commit

---
+++ src/com/example/cursinho/SobreActivity.java
@@ -0,0 +1,14 @@
+package com.example.cursinho;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SobreActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sobre);
+	}
+
+}
